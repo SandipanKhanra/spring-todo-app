ef98fa66e5f2aba9402b13bf21b501999f7bb594
Sandipan Khanra <sandipankhanra@gmail.com>
2024-06-03 18:57:30 +0530

    Initial login controller and login form implemented

---
+++ src/main/java/com/nitaigauranga/springboot/todoapp/login/LoginController.java
@@ -0,0 +1,12 @@
+package com.nitaigauranga.springboot.todoapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	@RequestMapping("login")
+	public String gotoLoginPage() {
+		return "login";
+	}
+}
